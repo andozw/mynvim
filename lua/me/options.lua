c4e7084344e845bb659956f723f114f1d00b51fe
Sean Andersen <seana@squareup.com>
2022-03-11 09:49:14 -0800

    Enable spell check

--- lua/me/options.lua
+++ lua/me/options.lua
@@ -1,5 +1,6 @@
 local cmd = vim.cmd
 local options = {
+    spell = true,                            -- enable spellcheck
     backup = false,                          -- creates a backup file
     clipboard = "unnamed",                   -- allows neovim to access the system clipboard
     cmdheight = 2,                           -- more space in the neovim command line for displaying messages
